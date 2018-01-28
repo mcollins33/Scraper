60dad9b6e8cc5abf7025a42cb9bb285cc21149d8
mcollins33 <collins.miki@gmail.com>
2018-01-28 13:36:36 -0500

    able to delete saved articles and refresh page

--- public/javascript/scraper.js
+++ public/javascript/scraper.js
@@ -2,42 +2,45 @@ $("#articlescrape").on("click", function(event) {
 
     $.ajax("/scrape", {
         type: "GET"
-    }).then(
-        function() {
-            // Reload the page to get the updated list
-            location.reload();
-        }
-    );
+    }).then(function() {
+        // Reload the page to get the updated list
+        location.reload();
+    });
 })
 
 $(".savearticle").on("click", function(event) {
 
-   var thisId = $(this).attr("data-id");
+    var thisId = $(this).attr("data-id");
 
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "PUT",
-    url: "/articles/" + thisId,
-    // data: {
-    //  saved: true
-    // }
-  })
-    // With that done
-    .then(function(data) {
-      // Log the response
-      console.log(data);
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+        method: "PUT",
+        url: "/articles/" + thisId,
+    }).then(function(data) {
+       location.reload();
     });
 });
 
 $(".savedarticles").on("click", function(event) {
 
-   $.ajax({
-    method: "GET",
-    url: "/articles",
-    })
-    // With that done
-    .then(function(data) {
-      // Log the response
-      console.log(data);
+    $.ajax({
+        method: "GET",
+        url: "/articles",
+    }).then(function(data) {
+        // Log the response
+        console.log(data);
     });
 })
+
+$(".delete").on("click", function(event) {
+
+    var thisId = $(this).attr("data-id");
+
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+        method: "DELETE",
+        url: "/articles/" + thisId,
+    }).then(function(data) {
+        location.reload();
+    });
+});
--- server.js
+++ server.js
@@ -93,6 +93,22 @@ app.put("/articles/:id", function(req, res) {
         .then(function(dbArticle) {
             // If we were able to successfully find Articles, send them back to the client
             console.log(dbArticle);
+             res.render("index", { article: dbArticle })
+        })
+        .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+
+});
+
+
+app.delete("/articles/:id", function(req, res) {
+    db.Article.findOneAndRemove({ _id: req.params.id })
+        .then(function(dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            console.log(dbArticle);
+            res.render("saved", { article: dbArticle })
         })
         .catch(function(err) {
             // If an error occurred, send it to the client
